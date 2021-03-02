a1d33e063848135684d3ae973d42924cd4e6fb26
Kimi <65819715+SirCodesAIot@users.noreply.github.com>
2021-03-02 12:58:59 -0800

    all commiteeedeededed

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -1,10 +1,8 @@
 import discord
 from discord.ext import commands
-# from discord.ext.commands import Bot
 
 from dotenv import load_dotenv
 # import csv
-# import asyncio
 
 import random as r
 import os
@@ -29,71 +27,50 @@ async def on_ready():
 async def on_command_error(ctx, message):
     if isinstance(message, Exception):
         await ctx.send(f"There is no such command!")
-    # await message.channel.send(f"There is no such command, {message.author.mention}!")
 
 
-@client.command(name='roll', help='Randomly rolls a dice for ya (numbers 1-6)! Do: roll <number of dices! (optional)>')
-async def roll(message, number=1):
+@client.event
+async def bad_word(message):
+    message.delete()
+    await message.channel.send(f"{message.author.mention} that word isn't allowed here!")
+
+
+@client.command(name='dice', help='Randomly rolls a dice for you(numbers 1-6)! Do: %roll <number of dices! (optional, '
+                                  'default is 1)>')
+async def dice(message, number=1):
     rolls = []
-    for i in range(number):
-        rolls.append(str(r.randint(1, 6)))
-    await message.channel.send(f"{message.author.mention}, your numbers are: {', '.join(rolls)}")
-
-
-# Kills crewmates
-@client.command(name='kill', help='Kills random amounts of people (Still in progress)')
-async def kill_things(message):
-    kills = r.choice([0, 0, 0, 1, 1, 2])
-    if kills == 0:
-        await message.channel.send(f'OOF! Nobody was there {message.author.mention} :(')
+    if number == 1:
+        await message.channel.send(f"{message.author.mention}, your number is {r.randint(1, 6)}")
+    elif number > 100:
+        await message.channel.send(f"{message.author.mention} your number is too big!")
     else:
-        if kills == 2:
-            await message.channel.send(f"{message.author.mention} got caught getting 2 kills!")
-        else:
-            await message.channel.send(f'\nWow, {message.author.mention} got {kills} kill!')
+        for i in range(number):
+            rolls.append(str(r.randint(1, 6)))
+        await message.channel.send(f"{message.author.mention}, your numbers are: {', '.join(rolls)}")
 
 
-# Finding things
-@client.command(name='search', help="\n\nPlace list: Electrical, Admin, Lower Engine, Upper "
-                                    "Engine, O2, Cafeteria, Navigation, Reactor, Medbay, Weapons, Shields, Storage"
-                                    "\n\nBased on the map Skield in the real game.")
-async def search(message, place_to_search=''):
-    things_you_can_get = ['knife', 'coins', 'massive Tongue', 'hair dyer', "crewmate's keycard", 'nothing',
-                          'missile',
-                          'nothing', 'nothing', 'ejected', 'gun']
-    places = ['Electrical', 'Admin', 'Lower Engine', 'Upper Engine', 'O2', 'Cafeteria', 'Navigation', 'Reactor',
-              'Medbay', 'Weapons', 'Shields', 'Storage']
-    if not place_to_search:
-        await message.channel.send("You have to enter a place you want to search!")
-    else:
-        if place_to_search and place_to_search.title() in places:
-            wat_u_get = r.choice(things_you_can_get)
-            wat_u_get_2 = r.choice(things_you_can_get)
-
-            if wat_u_get == 'ejected' or wat_u_get_2 == 'ejected':
-                await message.channel.send(f"{message.author.mention} got caught doing some 'sus' stuff, and got "
-                                           "ejected!")
-
-            elif wat_u_get == 'nothing':
-                await message.channel.send(f'{message.author.mention} got {wat_u_get_2}')
-
-            elif wat_u_get_2 == 'nothing':
-                await message.channel.send(f'{message.author.mention} got {wat_u_get}')
-
-            elif wat_u_get and wat_u_get_2 == 'nothing':
-                await message.channel.send(f'{message.author.mention} got nothing!')
-
-            else:
-                await message.channel.send(f'{message.author.mention} got a {wat_u_get} and a {wat_u_get_2}!')
-
-        else:
-            await message.channel.send(f'What are you even thinking {message.author.mention}, '
-                                       'that is not a available place to search/do your business.')
+@client.command(name='random', help='Chooses who can go first for you! Whoever gets the bigger the number, the person '
+                                    'who goes first! Do: %random! (Warning: you can only do this with 2 people!')
+async def random(message):
+    await message.channel.send(f"{message.author.mention} got {r.randint(1, 100)}, and your opponent got "
+                               f"{r.randint(1, 100)}")
 
 
-# @client.command(name='inv', help='Use this command to see what is inside your inventory! (Still in progress'
-#                                  'progress for storing your data')
-# async def inventory(message):
-#     await message.channel.send(f"Still in progress {message.author.mention}!(This will probably take a while...)")
+@client.command(name='youtube', help='Gives you the link for my Youtube Channel!')
+async def youtube(message):
+    await message.channel.send(f"{message.author.mention} here is the Youtube link:"
+                               f"\nhttps://www.youtube.com/channel/UC9J0m0MVZvpa27R8iMnL9bg")
+
+
+@client.command(name='patreon', help='Gives you the link for my Patreon page!')
+async def patreon(message):
+    await message.channel.send(f"{message.author.mention} here is the Patreon link:"
+                               f"\nhttps://www.patreon.com/KimiGets0FPS?fan_landing=true")
+
+
+@client.command(name='twitter', help='Gives you the link for my Twitter! Follow me!')
+async def twitter(message):
+    await message.channel.send(f"{message.author.mention} here is the Twitter link:"
+                               f"\nhttps://twitter.com/K0fps")
 
 client.run(TOKEN)
